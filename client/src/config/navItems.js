0f76d0c686270cd2529e336a65167c9006eb9d39
Devo-hacker <niladreebiharinayak@gmail.com>
2026-08-29 10:51:44 +0530

    removing settings

--- client/src/config/navItems.js
+++ client/src/config/navItems.js
@@ -6,7 +6,6 @@ import {
   FolderKanban,
   Link2,
   Database,
-  Settings,
 } from "lucide-react";
 
 export const navItems = [
@@ -16,6 +15,6 @@ export const navItems = [
   { icon: Map, label: "Roadmap", path: "/roadmap" },
   { icon: FolderKanban, label: "Projects", path: "/projects" },
   { icon: Link2, label: "Connections", path: "/connections" },
-  { icon: Database, label: "Storage", path: "/storage", badge: "New" },
-  { icon: Settings, label: "Settings", path: "/settings" },
+  { icon: Database, label: "Storage", path: "/storage"},
+//   { icon: Settings, label: "Settings", path: "/settings" },
 ];
